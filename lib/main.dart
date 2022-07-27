51add3802a3d90167e97e2d0073ad17b3a495c8d
channvuthy <channvuthyit@gmail.com>
2022-07-27 21:51:09 +0700

    Add title

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_desktop/config/config.dart';
 import 'package:flutter_desktop/constant/color.dart';
 import 'package:flutter_desktop/screen/loading/loading_screen.dart';
+import 'package:get/get.dart';
 import 'package:window_manager/window_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await windowManager.ensureInitialized();
   windowManager.maximize();
-  runApp(MaterialApp(
+  windowManager.setTitle(appName);
+  runApp(GetMaterialApp(
     theme:
         ThemeData(backgroundColor: WHITE_COLOR, brightness: Brightness.light),
     debugShowCheckedModeBanner: false,
-    home: LoadingScreen(),
+    initialRoute: "/loading_screen",
+    getPages: [GetPage(name: "/loading_screen", page: () => LoadingScreen())],
   ));
 }
