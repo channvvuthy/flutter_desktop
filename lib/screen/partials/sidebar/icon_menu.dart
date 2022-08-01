b57e426acf1634f4cb690de4788671f2554e976e
channvuthy <channvuthyit@gmail.com>
2022-08-01 17:28:42 +0700

    Add memu header

--- lib/screen/partials/sidebar/icon_menu.dart
+++ lib/screen/partials/sidebar/icon_menu.dart
@@ -6,36 +6,44 @@ import 'package:flutter_svg/svg.dart';
 
 class IconMenu extends StatelessWidget {
   final String title;
+  final VoidCallback onTap;
   final String icnUrl;
-  IconMenu({Key? key, required this.title, required this.icnUrl})
+  IconMenu(
+      {Key? key,
+      required this.title,
+      required this.icnUrl,
+      required this.onTap})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(children: [
-        Container(
-          alignment: Alignment.center,
-          width: 50,
-          height: 50,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50),
-              border: Border.all(color: BORDER_COLOR, width: 1)),
-          child: SvgPicture.asset(
-            icnUrl,
-            color: BORDER_COLOR,
-            width: 24,
+      child: InkWell(
+        onTap: onTap,
+        child: Column(children: [
+          Container(
+            alignment: Alignment.center,
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                border: Border.all(color: BORDER_COLOR, width: 1)),
+            child: SvgPicture.asset(
+              icnUrl,
+              color: BORDER_COLOR,
+              width: 24,
+            ),
           ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Text(
-          title,
-          style: TextStyle(fontSize: 11),
-          softWrap: false,
-        ),
-      ]),
+          SizedBox(
+            height: 4,
+          ),
+          Text(
+            title,
+            style: TextStyle(fontSize: 11),
+            softWrap: false,
+          ),
+        ]),
+      ),
     );
   }
 }
